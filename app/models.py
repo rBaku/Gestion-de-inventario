16315eb1fc8ea64019b80a65fc743b34b2cd285b
rBaku <rodolfoeduardoov@gmail.com>
2025-04-06 21:28:15 -0400

    modelos
    Define clases Producto y Usuario

---
+++ app/models.py
@@ -0,0 +1,12 @@
+class Producto:
+    def __init__(self, nombre, descripcion, cantidad, precio_unitario, categoria):
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.cantidad = cantidad
+        self.precio_unitario = precio_unitario
+        self.categoria = categoria
+
+class Usuario:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password  # Se debe hashear en la versión final
